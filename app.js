6eb9ac886b22f55aa19409e7242b60a0a47433d3
krishtina-boop <krishtinamaharjan8@gmail.com>
2025-12-22 19:52:40 +0545

    appsetup

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.json({ limit: "10mb" }));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use("/public", express.static("public", { type: "text/css" }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(
+  express.static("public", {
+    setHeaders: (res, path) => {
+      if (path.endsWith(".js")) {
+        res.setHeader("Content-Type", "text/javascript ");
+      }
+    },
+  })
+);
+const indexRoute = require("./routes/index.js");
+app.use("/", indexRoute);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
