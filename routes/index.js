4488551291a38a79a8a734e9be28d1a96bdc6afc
krishtina-boop <krishtinamaharjan8@gmail.com>
2025-12-23 07:55:56 +0545

    gen-ai api  integrated succesfully

--- routes/index.js
+++ routes/index.js
@@ -3,37 +3,56 @@ const router = express.Router();
 const googlegenai = require("@google/genai");
 
 router.get("/", async (req, res) => {
-  const api = process.env.GEN_API;
-  // console.log(api);
-  const ai = new googlegenai.GoogleGenAI({ apiKey: process.env.GEN_API });
-
-  // const chat = ai.chats.create({
-  //   model: "gemini-2.5-flash",
-  //   history: [
-  //     {
-  //       role: "user",
-  //       parts: [{ text: "Hello" }],
-  //     },
-  //     {
-  //       role: "model",
-  //       parts: [
-  //         { text: "Hi its great to meet you. What would you like to know" },
-  //       ],
-  //     },
-  //   ],
-  // });
-
-  // const response1 = await chat.sendMessage({
-  //   message: "I have 2 dogs in my house.",
-  // });
-  // console.log("Chat response 1:", response1.text);
-
-  // const response2 = await chat.sendMessage({
-  //   message: "How many paws are in my house?",
-  // });
-  // console.log("Chat response 2:", response2.text);
-
   res.render("index");
 });
 
+const ai = new googlegenai.GoogleGenAI({ apiKey: process.env.GEN_API });
+const qanda = {
+  hi: "hello",
+  hello: "hi, what do you want to know today?",
+  whatdoyoudo: "I help you understand about this web page",
+  bye: "have a nice day",
+};
+
+let chathistory = [];
+
+router.post("/chat", async (req, res) => {
+  const userMsg = req.body.message;
+
+  if (qanda[userMsg]) {
+    return res.json({
+      reply: qanda[userMsg],
+      source: "rule-based",
+    });
+  }
+  try {
+    const chat = ai.chats.create({
+      model: "gemini-2.5-flash",
+      history: chathistory,
+    });
+
+    const response = await chat.sendMessage({
+      message: userMsg,
+    });
+
+    chathistory.push(
+      {
+        role: "user",
+        parts: [{ text: userMsg }],
+      },
+      {
+        role: "user",
+        parts: [{ text: response.text }],
+      }
+    );
+
+    res.json({
+      reply: response.text,
+      source: "ai",
+    });
+  } catch (error) {
+    console.error(err);
+    res.status(500).json({ error: "Chatbot failed" });
+  }
+});
 module.exports = router;
